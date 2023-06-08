18eee69087321201a3b56382466cfa2473f78b08
Nephtaly Aniceta <n.aniceta@outlook.com>
2023-06-08 12:57:17 +0200

    Commit previous work

---
+++ packages/mapper/inc/mazeMap.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include <memory>
+
+struct Tile;
+
+using NeighbourTile = std::shared_ptr<const Tile>;
+
+struct Pos {
+        int x = 0;
+        int y = 0;
+};
+
+struct Tile {
+        Tile(Pos position) : pos(position) {}
+
+        bool visited = false;
+
+        Pos pos;
+
+        // Neighbours
+        NeighbourTile up    = nullptr;
+        NeighbourTile down  = nullptr;
+        NeighbourTile left  = nullptr;
+        NeighbourTile right = nullptr;
+};
+
+class MazeMap {
+    public:
+        MazeMap(Pos initialPosition)
+              : currentTile(std::make_shared<Tile>(initialPosition)){};
+        ~MazeMap();
+
+        std::shared_ptr<Tile> currentTile;
+
+    private:
+};
---
+++ packages/mapper/src/main.cpp
@@ -0,0 +1,17 @@
+﻿// mapper.cpp : Defines the entry point for the application.
+//
+
+// STL
+#ifndef _MSC_VER
+    #include "ros/ros.h"
+#endif 
+#include <iostream>
+
+// Local classes
+#include "mazeMap.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
