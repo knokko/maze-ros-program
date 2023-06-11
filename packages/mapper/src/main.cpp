5409230cf0c8f654bb32479fa401b0bc66b01c1b
Nephtaly Aniceta <n.aniceta@outlook.com>
2023-06-11 17:08:46 +0200

    left right correct

--- packages/mapper/src/main.cpp
+++ packages/mapper/src/main.cpp
@@ -11,17 +11,31 @@
 #include "mazeMap.h"
 
 int main() {
-    MazeMap maze(5, 3, 1);
+    MazeMap maze(5, 5, 1);
     maze.printMap();
 
     // Move
+    (*maze.currentTile()->left)  = true;
+    //(*maze.currentTile()->up)    = true;
+    (*maze.currentTile()->right) = true;
+    //(*maze.currentTile()->down)  = true;
     maze.currentPose.position += Position<double>{1.0, 1.0};
     std::cout << "\n\n\n"
               << std::endl;
     maze.printMap();
 
+    maze.currentPose.position    = Position<double>{2.0, 2.0};
+    (*maze.currentTile()->left)  = true;
+    //(*maze.currentTile()->up)    = true;
+    (*maze.currentTile()->right) = true;
+    //(*maze.currentTile()->down)  = true;
+    std::cout << "\n\n\n"
+              << std::endl;
+    maze.printMap();
+
     // Add walls
-    (*maze.currentTile()->down) = true;
+    //(*maze.currentTile()->right) = true;
+    //(*maze.currentTile()->down) = true;
     std::cout << "\n\n\n"
               << std::endl;
     maze.printMap();
--- packages/mapper/src/mazeMap.cpp
+++ packages/mapper/src/mazeMap.cpp
@@ -3,9 +3,14 @@
 MazeMap::MazeMap(size_t width, size_t height, double tileSize, Position<double> initialPosition)
       : tileSize(tileSize), currentPose({initialPosition, 0}) {
     // Initialize Walls
-    for(size_t y = 0; y < height + 1; y++)
+    for(size_t y = 0; y < height * 2 + 1; y++)
     {
-        wallMap.push_back(std::vector<WallPtr>(width + 1, std::make_shared<Wall>()));
+        std::vector<WallPtr> row;
+        for(size_t x = 0; x < width * 2 + (y % 2); x++)
+        {
+            row.push_back(std::make_shared<Wall>());
+        }
+        wallMap.push_back(row);
     }
 
     // Initialize Tiles
@@ -19,17 +24,17 @@ MazeMap::MazeMap(size_t width, size_t height, double tileSize, Position<double> 
             // First row needs to have the top walls additionally
             if(y == 0)
             {
-                tile->up = wallMap[y][x];
+                // tile->up = wallMap[0][x];
             }
             else
             {
-                tile->up = tileMap.back()[x]->down;
+                // tile->up = tileMap.back()[x]->down;
             }
 
             // The first column needs to have the left wall extra
             if(x == 0)
             {
-                tile->left = wallMap[y][x];
+                tile->left = wallMap[2 * y + 1][0];
             }
             else
             {
@@ -37,8 +42,8 @@ MazeMap::MazeMap(size_t width, size_t height, double tileSize, Position<double> 
             }
 
             // The rest of the walls are the same
-            tile->down  = wallMap[y][x + 1];
-            tile->right = wallMap[y + 1][x];
+            tile->right = wallMap[2 * y + 1][2 * (x + 1)];
+            // tile->down  = wallMap[2 * y + 1][2 * x];
 
             tileRow.push_back(tile);
         }
@@ -63,69 +68,60 @@ Position<int> MazeMap::currentTilePosition() const {
 void MazeMap::printMap() const {
     // Every tile is printed as a 3x3 square with the walls.
     // The middle of the square is a dot
-    for(size_t y = 0; y < tileMap.size(); y++)
+
+    // Print the rest
+    for(size_t y = 0; y < wallMap.size(); y++)
     {
-        // Print the top walls
-        for(size_t x = 0; x < tileMap[y].size(); x++)
+        for(size_t x = 0; x < wallMap[y].size(); x++)
         {
-            auto tile = tileMap[y][x];
-            if(tile->up->has_value())
+            const auto wall = wallMap.at(y).at(x).get();
+            // Odd rows contain only walls and the even rows have dots and walls
+            if(y % 2)
             {
-                std::cout << (tile->up->value() ? " - " : ".  .");
+                // This is a row with walls and points
+                // Again, we can split by parity
+                if(x % 2)
+                {
+                    // Print dot
+                    if((currentTilePosition().x * 2 + 1) == x && (currentTilePosition().y * 2 + 1) == y)
+                    {
+                        std::cout << "X";
+                    }
+                    else
+                    {
+                        std::cout << "o";
+                    }
+                }
+                else
+                {
+                    // Print wall
+                    if(wall->has_value())
+                    {
+                        std::cout << (wall->value() ? "|" : ":");
+                    }
+                    else
+                    {
+                        std::cout << " ";
+                    }
+                }
             }
+            // This is a row with only walls
             else
             {
-                std::cout << "   ";
-            }
-        }
-        std::cout << std::endl;
-        // Print the left walls and the dots
-        for(size_t x = 0; x < tileMap[y].size(); x++)
-        {
-            auto tile = tileMap[y][x];
-            // Left wall
-            if(tile->left->has_value())
-            {
-                std::cout << (tile->left->value() ? "|" : ".");
-            }
-            else
-            {
-                std::cout << " ";
-            }
+                // Print offset for first column
+                if(x == 0)
+                {
+                    std::cout << " ";
+                }
 
-            // Middle dot
-            if(currentTilePosition().x == x && currentTilePosition().y == y)
-            {
-                std::cout << "X";
-            }
-            else
-            {
-                std::cout << "o";
-            }
-
-            // Right wall
-            if(tile->right->has_value())
-            {
-                std::cout << (tile->right->value() ? "|" : ".");
-            }
-            else
-            {
-                std::cout << " ";
-            }
-        }
-        std::cout << std::endl;
-
-        // Print the bottom walls
-        for(size_t x = 0; x < tileMap[y].size(); x++)
-        {
-            auto tile = tileMap[y][x];
-            if(tile->down->has_value())
-            {
-                std::cout << tile->down->value() ? " - " : ".  .";
-            }
-            else
-            {
-                std::cout << "   ";
+                if(wall->has_value())
+                {
+                    std::cout << (wall->value() ? "-" : ".");
+                }
+                else
+                {
+                    std::cout << " ";
+                }
             }
         }
         std::cout << std::endl;
